e48b51b69ccbda233c0f0f2cfb34eec443c72390
Hawleywoo <alhawley21@gmail.com>
2020-11-10 21:02:13 -0700

    Image upload and form

--- src/Components/orderForm.js
+++ src/Components/orderForm.js
@@ -1,20 +1,60 @@
 import React from 'react'
 
+const initialState = {
+    name: '',
+    phoneNumber: '',
+}
+
 export default class OrderForm extends React.Component {
 
-    s
+    state = {
+        name: '',
+        phoneNumber: '',
+        imageFile: null,
+        src: null,
+    }
 
-    const handleChange = (event) => {
+    handleChange = (event) => {
         let { name } = event.target
         let value = event.target.value
-        setPhoneNumber(value)
+        this.setState({
+            [name]: value
+        })
     }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.addOrder(this.state)
+        this.resetState()
+    }
+
+    resetState = () => {
+        this.setState(initialState)
+    }
+
+    handleFileChange = (event) => {
+        this.setState({
+            imageFile: event.target.files[0],
+            src: URL.createObjectURL(event.target.files[0])
+        })
+    }
+
+    fileUploadHandler = () => {
+
+    }
+
     render(){
         return(
-            <form>
-                <input name="name" value={name} placeholder='Name...' onChange={handleChange} />
-                <input name="phoneNumber" value={phoneNumber} placeholder='Phone Number (555-555-5555)' onChange={handleChange} />
-            </form>
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input name="name" value={this.state.name} placeholder='Name...' onChange={this.handleChange} />
+                    <input name="phoneNumber" value={this.state.phoneNumber} placeholder='Phone Number (555-555-5555)' onChange={this.handleChange} />
+                    <input type="textarea" />
+                    <input type="file" multiple onChange={this.handleFileChange} />
+                    <input type="submit" placeholder="Submit Order" />
+                </form>
+                <img src={this.state.src}  alt="Uploaded" />
+            </div>
         )
     }
 }
