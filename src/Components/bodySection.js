382b212f13d348d3bd3de973e13c288e1d4acde9
Hawleywoo <alhawley21@gmail.com>
2020-11-25 23:46:47 -0700

    conditional render order of sections

--- src/Components/bodySection.js
+++ src/Components/bodySection.js
@@ -3,13 +3,22 @@ import React from 'react'
 export default function BodySection(props){
     return (
         <div className="body-section">
-            <img className="section-image" src={props.imageSource} alt="images or something" />
-
-            <div className="description" >
-                <h3>{props.title}</h3>
-                <hr/>
-                <p>{props.description}</p>
-            </div>
+            {props.order % 2 !== 0 
+                ?<div className="description" >
+                    <h3>{props.title}</h3>
+                    <hr/>
+                    <p>{props.description}</p>
+                </div>
+                :<img className="section-image" src={props.imageSource} alt="images or something" />
+            }
+            {props.order % 2 == 0 
+                ?<div className="description" >
+                    <h3>{props.title}</h3>
+                    <hr/>
+                    <p>{props.description}</p>
+                </div>
+                :<img className="section-image" src={props.imageSource} alt="images or something" />
+            }
         </div>
     )
 }
