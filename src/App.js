00bce2a861fa44860901d9332fdf327ceeed746a
Hawleywoo <alhawley21@gmail.com>
2020-12-04 23:00:23 -0700

    Handle error

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import OrderForm from './Components/orderForm'
 import React from 'react'
 import BodySection from './Components/bodySection'
 import Login from './Components/login'
-import { 
-  BrowserRouter as Router, 
-  Link, 
-  Route, 
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
   Switch
 } from 'react-router-dom'
 
@@ -40,32 +40,32 @@ class App extends React.Component {
 
 
 
-  render(){
+  render() {
     return (
-      <div className="App">
-        <Header />
-        <img className="banner-img" src="https://images-gmi-pmc.edge-generalmills.com/087d17eb-500e-4b26-abd1-4f9ffa96a2c6.jpg" alt="cookies" />
-        <hr></hr>
-        <div className="main-body">
-          <BodySection order={1} title="About Me" description={aboutUs} imageSource="https://annabanana.co/wp-content/uploads/2020/03/Easter-Sugar-Cookies-23.jpg" />
-          <BodySection order={2} title="Something Else" description={nextSection} imageSource="https://4.bp.blogspot.com/-SjHtd1ecT1Y/XF8Y8oH-e4I/AAAAAAACv6A/JTUIIk4NqUA2Qb0x8ywOmK75KwA-bUmowCLcBGAs/s1600/Heart%2BShaped%2BLofthouse%2BStyle%2BSugar%2BCookies.jpg" />
-          <div>
-            <a>pricing sheet</a>
-            <a>order cookies</a>
-            <OrderForm addOrder={this.addOrder} />
-          </div>
-          <Router>
-            <Link to={/admin} > Admin </Link >
-            <Switch>
-              <Route path="/admin">
-                <Login />
-              </Route>
+      <Router>
+        <div className="App">
+          <Header />
+          <img className="banner-img" src="https://images-gmi-pmc.edge-generalmills.com/087d17eb-500e-4b26-abd1-4f9ffa96a2c6.jpg" alt="cookies" />
+          <hr></hr>
+          <div className="main-body">
+            <BodySection order={1} title="About Me" description={aboutUs} imageSource="https://annabanana.co/wp-content/uploads/2020/03/Easter-Sugar-Cookies-23.jpg" />
+            <BodySection order={2} title="Something Else" description={nextSection} imageSource="https://4.bp.blogspot.com/-SjHtd1ecT1Y/XF8Y8oH-e4I/AAAAAAACv6A/JTUIIk4NqUA2Qb0x8ywOmK75KwA-bUmowCLcBGAs/s1600/Heart%2BShaped%2BLofthouse%2BStyle%2BSugar%2BCookies.jpg" />
+            <div>
+              <a>pricing sheet</a>
+              <a>order cookies</a>
+              <OrderForm addOrder={this.addOrder} />
+            </div>
 
-            </Switch>
+            <Link to="/admin"> Admin </Link >
+              <Switch>
+                <Route path="/admin">
+                  <Login />
+                </Route>
 
-          </Router>
-        </div>
+              </Switch>
+              </div>
       </div>
+      </Router>
     );
   }
 }
