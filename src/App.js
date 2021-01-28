0541bfe521754d14fbb549120b0f720cd9347c2e
Hawleywoo <alhawley21@gmail.com>
2021-01-27 20:26:19 -0700

    use context, refs on sign up

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import BodySection from './Components/bodySection'
 import Login from './Components/login'
 import OrdersContainer from './Components/ordersContainer'
 import SignUp from './Components/signUp'
+import { AuthProvider } from './Contexts/authContext'
 
 import {
   BrowserRouter as Router,
@@ -60,32 +61,34 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-        <div className="App">
-          <Header />
-          <img className="banner-img" src="https://images-gmi-pmc.edge-generalmills.com/087d17eb-500e-4b26-abd1-4f9ffa96a2c6.jpg" alt="cookies" />
-          <hr></hr>
-          <div className="main-body">
-            <BodySection order={1} title="About Me" description={aboutUs} imageSource="https://annabanana.co/wp-content/uploads/2020/03/Easter-Sugar-Cookies-23.jpg" />
-            <BodySection order={2} title="Something Else" description={nextSection} imageSource="https://4.bp.blogspot.com/-SjHtd1ecT1Y/XF8Y8oH-e4I/AAAAAAACv6A/JTUIIk4NqUA2Qb0x8ywOmK75KwA-bUmowCLcBGAs/s1600/Heart%2BShaped%2BLofthouse%2BStyle%2BSugar%2BCookies.jpg" />
-            <div>
-              <a>pricing sheet</a>
-              <a>order cookies</a>
-              <OrderForm addOrder={this.addOrder} />
-            </div>
-
-            <Link to="/admin"> Admin </Link >
-              <Switch>
-                <Route path="/admin">
-                  <Login />
-                  {this.state.user.userName
-                    ? <h2>Welcome {this.state.user.userName}</h2> 
-                    : <SignUp signUp={this.signUp}/>  
-                  }
-                </Route>
-
-              </Switch>
+        <AuthProvider>
+          <div className="App">
+            <Header />
+            <img className="banner-img" src="https://images-gmi-pmc.edge-generalmills.com/087d17eb-500e-4b26-abd1-4f9ffa96a2c6.jpg" alt="cookies" />
+            <hr></hr>
+            <div className="main-body">
+              <BodySection order={1} title="About Me" description={aboutUs} imageSource="https://annabanana.co/wp-content/uploads/2020/03/Easter-Sugar-Cookies-23.jpg" />
+              <BodySection order={2} title="Something Else" description={nextSection} imageSource="https://4.bp.blogspot.com/-SjHtd1ecT1Y/XF8Y8oH-e4I/AAAAAAACv6A/JTUIIk4NqUA2Qb0x8ywOmK75KwA-bUmowCLcBGAs/s1600/Heart%2BShaped%2BLofthouse%2BStyle%2BSugar%2BCookies.jpg" />
+              <div>
+                <a>pricing sheet</a>
+                <a>order cookies</a>
+                <OrderForm addOrder={this.addOrder} />
               </div>
-      </div>
+
+              <Link to="/admin"> Admin </Link >
+                <Switch>
+                  <Route path="/admin">
+                    <Login />
+                    {this.state.user.userName
+                      ? <h2>Welcome {this.state.user.userName}</h2> 
+                      : <SignUp signUp={this.signUp}/>  
+                    }
+                  </Route>
+
+                </Switch>
+                </div>
+        </div>
+      </AuthProvider>
       </Router>
     );
   }
--- src/Components/signUp.js
+++ src/Components/signUp.js
@@ -1,37 +1,49 @@
-import React from 'react'
+import React , { useRef,useState} from 'react'
+import { useAuth } from '../Contexts/authContext'
 
-let defaultSignUp = {
-    userName: '',
-    password: ''
-}
 
-export default class SignUp extends React.Component{
-    state = {
-        userName: '',
-        password: '',
-    }
+export default function SignUp(){
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
 
-    handleChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmRef = useRef()
 
-    handleSubmit = (event) => {
+    const { signUp } = useAuth()
+
+
+    // const handleChange = (event) => {
+    //     this.setState({
+    //         [event.target.name]: event.target.value
+    //     })
+    // }
+
+    const handleSubmit = async (event) => {
         event.preventDefault()
-        this.props.signUp(this.state)
-        this.setState({defaultSignUp})
+
+        if(passwordRef.current.value !== passwordConfirmRef.current.value){
+            return setError('Passwords do not match')
+        }
+
+        try {
+            setError('')
+            setLoading(true)
+            await signUp(emailRef.current.value, passwordRef.current.value)
+        } catch {
+            setError('Failed to create an account')
+        }
+
+        setLoading(false)
     }
 
-    render(){
         return(
-            <form className="signUp" onSubmit={this.handleSubmit} >
-                <label>User Name</label>
-                <input name="userName"  value={this.state.userName} placeholder="UserName..." onChange={this.handleChange}/>
+            <form className="signUp" onSubmit={handleSubmit} >
+                <label>Email</label>
+                <input name="email" ref={emailRef} value={email} placeholder="Email" onChange={this.handleChange}/>
                 <label>Password</label>
-                <input name="password" value={this.state.password} type="password"  placeholder="Password..." onChange={this.handleChange} />
-                <input type="submit" value="Sign Up" />
+                <input name="password" ref={passwordRef} value={password} type="password"  placeholder="Password..." onChange={this.handleChange} />
+                <input type="submit" disabled={loading} value="Sign Up" />
             </form>
         )
-    }
 }
