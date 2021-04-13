773c46d49d0a77e996cb31e196c68e2c9618e8b7
Hawleywoo <alhawley21@gmail.com>
2021-04-12 22:22:44 -0600

    Scroll animation

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,19 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useLayoutEffect } from 'react'
 import logo from '../images/Decadent_Dough_trans.jpg'
 
 export default function Header (){
-    const [ toggle, setToggle ] = useState(false)
+    const [ toggle, setToggle ] = useState(true)
+    const headerRef = useRef()
 
-    const handleScroll = (event) => {
-        console.log(event.target)
-        console.log(event.target.detail)
-        setToggle(true)
+    useLayoutEffect(()=>{
+        const topPosition = element => headerRef.current.getBoundingClientRect().top
+
+        const onScroll = () => {
+            const scrollPosition = window.scrollY + window.innerHeight
+            console.log(scrollPosition, 'scroll position')
+            console.log(window.innerHeight, 'inner')
+            console.log(window.scrollY, 'sroll Y')
+            
+            const elPosition = topPosition(headerRef.current)
+            
+            console.log(elPosition, 'element Y')
+            if(window.scrollY > elPosition){
+                setToggle(false)
+            } else if(window.scrollY < elPosition){
+                setToggle(true)
+            }
+
+        }
         
-    }
+        window.addEventListener('scroll', onScroll)
+        return () => window.removeEventListener('scroll', onScroll) 
+    }, [])
+
+
 
     return(
-        <div className="header__parent" onScroll={handleScroll} >
-            <header className="header" >
+        <div className="header__parent" >
+            <header ref={headerRef} className={`header ${toggle ? '' : `header__scrolled`}`}>
                 <img src={logo} alt="Decadent Dough" className="head-logo"/>
             </header>
         </div>
